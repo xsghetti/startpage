c6416b9c8ecc9771bf2cd3d943c87fe9da5fe685
Dante <150515748+xsghetti@users.noreply.github.com>
2024-03-28 13:28:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function showTime() {
 
 	document.getElementById(
 		"date"
-	).innerHTML = `${today}, ${hour}:${minute}:${second} | ${day}/${month}/${year}`;
+	).innerHTML = `${today}, ${hour}:${minute}:${second} | ${month}/${day}/${year}`;
 	setTimeout(showTime, 0);
 }
 
